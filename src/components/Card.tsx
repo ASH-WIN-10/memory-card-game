a935302a6e200e4ea420e4db94a4dbc12fb8dc73
Ashvin <ashvinjangid10@gmail.com>
2025-02-09 23:24:05 +0530

    basic game logic

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,20 @@
 import { Character } from "@/fetchCharacters"
 
-export default function Card({ character }: { character: Character }) {
+type PropsType = {
+    character: Character
+    handleClick: (e: React.MouseEvent<HTMLDivElement>) => void
+}
+
+export default function Card({ character, handleClick }: PropsType) {
     return (
         <div
             className="bg-cover bg-center w-56 h-72 flex items-end rounded-xl cursor-pointer hover:scale-105 transition-transform duration-300"
             style={{
                 backgroundImage: `url(${character.image_url})`,
                 boxShadow: "rgba(0, 0, 0, 0.4) 3px 5px 8px",
-            }}>
+            }}
+            data-mal_id={character.mal_id}
+            onClick={handleClick}>
             <span className="w-full text-center text-xl font-bold text-white p-4 bg-black/50 backdrop-blur-md rounded-b-xl">
                 {character.name}
             </span>
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,11 +1,36 @@
+import { useState } from "react"
 import { Character } from "@/fetchCharacters"
 import Card from "@/components/Card"
 
 export default function Cards({ characters }: { characters: Character[] }) {
+    const [seen, setSeen] = useState<number[]>([])
+    characters = characters.sort(() => Math.random() - 0.5)
+    console.log(characters)
+
+    function handleClick(e: React.MouseEvent<HTMLDivElement>) {
+        const mal_id = parseInt(e.currentTarget.dataset.mal_id!)
+
+        if (seen.includes(mal_id)) {
+            alert("Game Over!")
+            setSeen([])
+            return
+        }
+        setSeen([...seen, mal_id])
+    }
+
+    if (seen.length === characters.length) {
+        alert("You win!")
+        setSeen([])
+    }
+
     return (
         <div className="p-8 w-4/5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 gap-y-5 m-auto justify-items-center">
             {characters.map((character) => (
-                <Card key={character.mal_id} character={character} />
+                <Card
+                    key={character.mal_id}
+                    character={character}
+                    handleClick={handleClick}
+                />
             ))}
         </div>
     )
