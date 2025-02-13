e31c6b52396577c3bd547f94cc9720835a344fd7
Ashvin <ashvinjangid10@gmail.com>
2025-02-13 20:28:15 +0530

    change website name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,9 @@ export default function Header({ score }: { score: number }) {
             className="p-4 text-white bg-gray-900 flex flex-wrap gap-2 justify-center sm:justify-between items-center"
             style={{ boxShadow: "rgba(0, 0, 0, 0.4) 3px 5px 8px" }}>
             <div className="flex flex-col gap-1">
-                <span className="text-5xl font-extrabold">Memory Card</span>
+                <span className="text-5xl font-extrabold">
+                    Click &amp; Remember
+                </span>
                 <span className="text-sm">
                     - Get points by clicking on an image but don't click on any
                     more than once!
