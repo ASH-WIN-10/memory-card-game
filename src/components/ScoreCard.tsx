7d9b7c02f5586d57f094d813adff2d0d916db534
Ashvin <ashvinjangid10@gmail.com>
2025-02-13 17:12:42 +0530

    show the last 'highest score' to the user

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react"
+
 export default function ScoreCard({ score }: { score: number }) {
+    const [highest, setHighest] = useState(0)
+
+    useEffect(() => {
+        if (score > highest) {
+            setHighest(score)
+        }
+    }, [score, highest])
+
     return (
         <div className="text-xl text-white text-center font-bold p-2 pl-4 pr-4 bg-gray-700 rounded-xl">
             <span>Score: {score}</span>
             <br />
-            <span>Highest: 0</span>
+            <span>Highest: {highest}</span>
         </div>
     )
 }
