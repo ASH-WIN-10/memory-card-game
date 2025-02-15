a59427c4656c3267ce935da35518c3020555f489
Ashvin <ashvinjangid10@gmail.com>
2025-02-15 22:20:13 +0530

    add a difficulty selector

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,49 @@ import Header from "@/components/Header"
 import Background from "@/assets/background.jpg"
 import { Loader2 } from "lucide-react"
 
+function ModeSelector({
+    setSeletorMode,
+    setMaxScore,
+}: {
+    setSeletorMode: React.Dispatch<React.SetStateAction<boolean>>
+    setMaxScore: React.Dispatch<React.SetStateAction<number>>
+}) {
+    function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+        const mode = e.currentTarget.textContent?.toLowerCase()
+        setSeletorMode(false)
+        if (mode === "easy") {
+            setMaxScore(6)
+        } else if (mode === "medium") {
+            setMaxScore(9)
+        } else if (mode === "hard") {
+            setMaxScore(12)
+        }
+    }
+
+    return (
+        <div className="h-full w-full flex flex-col gap-4 justify-center items-center backdrop-blur-lg">
+            <span className="text-white text-5xl font-extrabold text-center">
+                Choose difficulty:
+            </span>
+            <div className="flex flex-wrap justify-center gap-4">
+                {["Easy", "Medium", "Hard"].map((mode) => (
+                    <button
+                        className="text-white w-64 text-4xl font-extrabold bg-black/20 backdrop-blur-lg p-6 rounded-xl"
+                        onClick={handleClick}>
+                        {mode}
+                    </button>
+                ))}
+            </div>
+        </div>
+    )
+}
+
 function App() {
     const [score, setScore] = useState(0)
+    const [maxScore, setMaxScore] = useState(0)
     const [characters, setCharacters] = useState<Character[]>([])
     const [loading, setLoading] = useState(true)
+    const [selectorMode, setSelectorMode] = useState(true)
 
     useEffect(() => {
         charactersPromise.then((charactersArr) => {
@@ -25,7 +64,7 @@ function App() {
     if (loading) {
         return (
             <div
-                className="text-white flex flex-col gap-4 bg-center bg-cover h-full sm:h-screen z-10"
+                className="text-white flex flex-col gap-4 bg-center bg-cover h-full sm:h-screen"
                 style={{ backgroundImage: `url(${Background})` }}>
                 <Header score={score} />
                 <div className="absolute top-0 h-full w-full grid place-items-center backdrop-blur-md">
@@ -35,12 +74,31 @@ function App() {
         )
     }
 
+    if (selectorMode) {
+        return (
+            <div
+                className="grid place-items-center bg-center bg-cover h-screen"
+                style={{ backgroundImage: `url(${Background})` }}>
+                <ModeSelector
+                    setSeletorMode={setSelectorMode}
+                    setMaxScore={setMaxScore}
+                />
+            </div>
+        )
+    }
+
     return (
         <div
             className="flex flex-col gap-4 bg-center bg-cover h-full sm:h-screen"
             style={{ backgroundImage: `url(${Background})` }}>
             <Header score={score} />
-            <Cards characters={characters} score={score} setScore={setScore} />
+            <Cards
+                characters={characters}
+                score={score}
+                setScore={setScore}
+                maxScore={maxScore}
+                setSelectorMode={setSelectorMode}
+            />
         </div>
     )
 }
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -5,20 +5,29 @@ import Card from "@/components/Card"
 function Dialog({
     result,
     dialogRef,
+    setSelectorMode,
 }: {
     result: string
     dialogRef: React.RefObject<HTMLDialogElement | null>
+    setSelectorMode: React.Dispatch<React.SetStateAction<boolean>>
 }) {
+    function handleClick() {
+        setSelectorMode(true)
+        dialogRef.current?.close()
+    }
+
     return (
         <dialog
             ref={dialogRef}
             className="w-4/5 md:w-1/2 lg:w-1/3 p-10 bg-black/20 text-white backdrop-blur-lg rounded-xl backdrop:backdrop-blur-sm">
-            <form method="dialog" className="flex flex-col items-center gap-6">
+            <div className="flex flex-col items-center gap-6">
                 <span className="text-4xl font-extrabold">{result}</span>
-                <button className="text-lg font-bold bg-black/40 py-3 px-6 rounded-xl">
+                <button
+                    className="text-lg font-bold bg-black/40 py-3 px-6 rounded-xl"
+                    onClick={handleClick}>
                     Play again
                 </button>
-            </form>
+            </div>
         </dialog>
     )
 }
@@ -61,9 +70,17 @@ type CardsPropsType = {
     characters: Character[]
     score: number
     setScore: React.Dispatch<React.SetStateAction<number>>
+    maxScore: number
+    setSelectorMode: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export default function Cards({ characters, score, setScore }: CardsPropsType) {
+export default function Cards({
+    characters,
+    score,
+    setScore,
+    maxScore,
+    setSelectorMode,
+}: CardsPropsType) {
     const [jumbledCharacters, setJumbledCharacters] = useState<Character[]>([])
     const [seen, setSeen] = useState<number[]>([])
 
@@ -86,7 +103,7 @@ export default function Cards({ characters, score, setScore }: CardsPropsType) {
     }
 
     useEffect(() => {
-        if (score === characters.length) {
+        if (score === maxScore) {
             setScore(0)
             setSeen([])
             setResult("You win!")
@@ -94,11 +111,15 @@ export default function Cards({ characters, score, setScore }: CardsPropsType) {
         }
 
         setJumbledCharacters(getJumbledCharacters(characters, seen))
-    }, [score, setScore, characters, seen])
+    }, [score, setScore, maxScore, characters, seen, setSelectorMode])
 
     return (
         <div className="flex-1 flex pb-6 px-4 gap-10 flex-wrap items-center justify-center">
-            <Dialog result={result} dialogRef={dialogRef} />
+            <Dialog
+                result={result}
+                dialogRef={dialogRef}
+                setSelectorMode={setSelectorMode}
+            />
             {jumbledCharacters.map((character) => (
                 <Card
                     key={character.mal_id}
