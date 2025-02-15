899c37fa0c05b05dd0082d2c3f4cfe2ac3e9871e
Ashvin <ashvinjangid10@gmail.com>
2025-02-15 18:27:22 +0530

    better loading state

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,33 @@ import charactersPromise, { Character } from "@/fetchCharacters"
 import Cards from "@/components/Cards"
 import Header from "@/components/Header"
 import Background from "@/assets/background.jpg"
+import { Loader2 } from "lucide-react"
 
 function App() {
     const [score, setScore] = useState(0)
     const [characters, setCharacters] = useState<Character[]>([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         charactersPromise.then((charactersArr) => {
             if (!charactersArr) throw new Error("Failed to fetch characters")
+
+            const img = new Image()
+            img.src = charactersArr[0].image_url
+            img.onload = () => setLoading(false)
+
             setCharacters(charactersArr)
         })
     }, [])
 
-    if (characters.length === 0) {
+    if (loading) {
         return (
-            <div style={{ backgroundImage: `url(${Background})` }}>
+            <div
+                className="text-white flex flex-col gap-4 bg-center bg-cover h-full sm:h-screen z-10"
+                style={{ backgroundImage: `url(${Background})` }}>
                 <Header score={score} />
                 <div className="absolute top-0 h-full w-full grid place-items-center backdrop-blur-md">
-                    <span className="text-3xl font-extrabold">Loading...</span>
+                    <Loader2 size={64} className="animate-spin" />
                 </div>
             </div>
         )
