30addf1ed1b48f9ad46c8cc7ee97d895df66103f
Ashvin <ashvinjangid10@gmail.com>
2025-02-15 23:56:02 +0530

    add loader for individual card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,6 @@
 import { Character } from "@/scripts/fetchCharacters"
+import { Loader2 } from "lucide-react"
+import { useState, useEffect } from "react"
 
 type PropsType = {
     character: Character
@@ -6,6 +8,22 @@ type PropsType = {
 }
 
 export default function Card({ character, handleClick }: PropsType) {
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const img = new Image()
+        img.src = character.image_url
+        img.onload = () => setLoading(false)
+    }, [character.image_url])
+
+    if (loading) {
+        return (
+            <div className="w-56 h-72 grid place-items-center backdrop-blur-xl rounded-xl cursor-pointer hover:scale-105 transition-transform duration-300">
+                <Loader2 size={64} className="text-white animate-spin" />
+            </div>
+        )
+    }
+
     return (
         <div
             className="bg-cover bg-center w-56 h-72 flex items-end rounded-xl cursor-pointer hover:scale-105 transition-transform duration-300"
